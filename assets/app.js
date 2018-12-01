4a4d5ea3e9f0b37af2594f472d495ce58882b467
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2018-12-01 07:05:45 -0500

    changing to new portfolio

---
+++ assets/app.js
@@ -0,0 +1,40 @@
+// call auth 
+var userAuth = firebase.auth();
+// use auth 
+userAuth.signInAnonymously().catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+    // ...
+});
+
+userAuth.onAuthStateChanged(function (user) {
+    if (user) {
+        // User is signed in.
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        // ...
+    } else {
+        // User is signed out.
+        // ...
+    }
+    // ...
+});
+// call firebase rt db
+var db = firebase.database();
+var contactRef = db.ref();
+// add onsubmit handler 
+var contactForm = document.getElementById('formContactMe');
+contactForm.onsubmit = function (event) {
+    var formData = new FormData(contactForm);
+    // call firebase
+    contactRef.push({
+        name: formData.get('inputName'),
+        email: formData.get('inputEmail'),
+        comments: formData.get('inputComments')
+    })
+}
+
+// change linked in images for smaller screens 
